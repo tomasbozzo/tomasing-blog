400d315661c69bfe37d6e4482ce36271eeaf5fe7
Tomas Bozzo <tomasbozzo@gmail.com>
2019-01-09 19:45:05 +0100

    Issue #1 Create a home page.

---
+++ src/test/java/com/tomasing/blog/web/controller/HomeControllerTest.java
@@ -0,0 +1,25 @@
+package com.tomasing.blog.web.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHome() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+}
